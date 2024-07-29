854fd44b55b41bdb1c3d1a30d5f624d219cbad93
Junha Lee <junha.lee1@cj.net>
2024-07-29 18:06:31 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const mongoose = require('mongoose');
+mongoose
+  .connect(
+    'mongodb+srv://bluejunha:Dthn7Q9rPY1gNWev@boilerplate.0feloyk.mongodb.net/?retryWrites=true&w=majority&appName=boilerplate'
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+//mongodb+srv://bluejunha:Dthn7Q9rPY1gNWev@boilerplate.0feloyk.mongodb.net/
